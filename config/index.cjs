20d607ce56ce97f88cc6c50620176042ec1d7407
tenghaoman <65745225+tenghaoman@users.noreply.github.com>
2023-03-08 18:14:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogSUI6B-EprLFXeO09cI6iwuWfuA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ruvbJg19DUg9mNBIxSrjBKKxSlPkmblJugCxymElb-8',
+      useTemplateId: 'htWT75cag9OS28NQN3mMwl26gi_UgQwkQjGnNHatO0Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-20',
       festivals: [
