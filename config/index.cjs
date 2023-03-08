dda26cffa7494fe1cdf4fe7aa028204e28af346d
tenghaoman <65745225+tenghaoman@users.noreply.github.com>
2023-03-08 18:23:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ogSUI6B-EprLFXeO09cI6iwuWfuA',
+      id: 'ogSUI6F0eN06DaDgL-9CHzVlxA4U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'WdzzCDcCB8ojcxg62AQ5NYQegL6vDivbLNLPmQ-B71o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
